64191a6e07ede8f552cee24bdeaf4ede005cfbd3
DanielGals <janjangallaron@gmail.com>
2023-06-02 23:13:23 +0800

    Initialize Main Activity
    asd

--- app/src/main/java/com/example/sti_project/MainActivity.java
+++ app/src/main/java/com/example/sti_project/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.sti_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, pass;
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello ako maba");
-        System.out.println("Mukha ako maba");
+
+        email = (EditText) findViewById(R.id.Email);
+        pass = (EditText) findViewById(R.id.Password);
+        login = (Button) findViewById(R.id.Login);
+        register = (Button) findViewById(R.id.Register);
+
+
+
+
+
+
+
     }
 }
